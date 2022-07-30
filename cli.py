3eef7618ee1e448edc1b161a2b99c5c83c75cb9b
zhangke <zhangke@zdns.cn>
2022-07-30 15:49:10 +0800

    Perfect

--- cli.py
+++ cli.py
@@ -11,95 +11,97 @@ import yaml
 
 class Cli:
     def __init__(self, conf):
-        self.logger = logging.getLogger()
-        self.conf = conf
-        self.app_conns = []
-        self.trans_conns = []
+        self._logger = logging.getLogger()
+        self._conf = conf
+        self._app_conns = []
+        self._trans_conns = []
         try:
-            self.server_host = self.conf["server"]["host"]
+            self._server_host = self._conf["server"]["host"]
         except Exception as e:
-            self.logger.info("get server host from config fail: {0}".format(e))
-            raise e
+            raise SystemExit(e)
         try:
-            self.server_port = self.conf["server"]["port"]
+            self._server_port = self._conf["server"]["port"]
         except Exception as e:
-            self.logger.info("get server port from config fail: {0}".format(e))
-            raise e
+            raise SystemExit(e)
         try:
-            self.local_port = self.conf["local"]["port"]
+            self._local_port = self._conf["local"]["port"]
         except Exception as e:
-            self.logger.info("get local port from config fail: {0}".format(e))
-            raise e
+            raise SystemExit(e)
         try:
-            self.local_host = self.conf["local"]["host"]
+            self._local_host = self._conf["local"]["host"]
         except Exception as e:
-            self.logger.info("get local host from config fail: {0}".format(e))
-            raise e
+            raise SystemExit(e)
 
     def __str__(self):
-        return str(self.conf)
+        return str(self._conf)
 
     def start(self):
+        self._logger.info("start client")
         listen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listen.bind((self.local_host, self.local_port))
+        listen.bind((self._local_host, self._local_port))
         listen.listen()
+        self._logger.info(
+            "listen app conn {}:{}".format(self._local_host, self._local_port))
         try:
             while 1:
                 app_conn, addr = listen.accept()
-                self.app_conns.append(app_conn)
-                threading.Thread(target=self.__handle_app_conn, args=(app_conn,)).start()
+                self._app_conns.append(app_conn)
+                threading.Thread(target=self._handle_app_conn, args=(app_conn,)).start()
         except BaseException as e:
+            self._logger.error(
+                "closing listen app conn {}:{}, {}".format(self._local_host, self._local_port, e))
             try:
                 listen.shutdown(socket.SHUT_RDWR)
                 listen.close()
             except BaseException as ee:
                 ...
-            self.__when_listen_close()
+            self._when_listen_close()
             raise e
 
-    def __when_listen_close(self):
-        for app_conn in self.app_conns:
+    def _when_listen_close(self):
+        for app_conn in self._app_conns:
             try:
                 app_conn.shutdown(socket.SHUT_RDWR)
                 app_conn.close()
             except BaseException as e:
                 ...
-        self.app_conns = []
-        for trans_conn in self.trans_conns:
+        self._app_conns = []
+        for trans_conn in self._trans_conns:
             try:
                 trans_conn.shutdown(socket.SHUT_RDWR)
                 trans_conn.close()
             except BaseException as e:
                 ...
-        self.trans_conns = []
+        self._trans_conns = []
 
-    def __handle_app_conn(self, app_conn: socket.socket):
+    def _handle_app_conn(self, app_conn: socket.socket):
         app_conn_addr = app_conn.getpeername()
         trans_conn = socket.socket()
         try:
             trans_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            trans_conn.connect((self.server_host, self.server_port))
+            trans_conn.connect((self._server_host, self._server_port))
             trans_conn_addr = trans_conn.getsockname()
-            self.trans_conns.append(trans_conn)
-            self.logger.info(
-                "accept {}:{} <-> {}:{}".format(app_conn_addr[0], app_conn_addr[1], self.local_host, self.local_port))
-            self.logger.info("relay {}:{} <-> {}:{}".format(trans_conn_addr[0], trans_conn_addr[1], self.server_host,
-                                                            self.server_port))
-            threading.Thread(target=self.__handle_trans_conn, args=(app_conn, trans_conn)).start()
+            self._trans_conns.append(trans_conn)
+            self._logger.info(
+                "accept {}:{} <-> {}:{}".format(app_conn_addr[0], app_conn_addr[1], self._local_host, self._local_port))
+            self._logger.info("relay {}:{} <-> {}:{}".format(trans_conn_addr[0], trans_conn_addr[1], self._server_host,
+                                                             self._server_port))
+            threading.Thread(target=self._handle_trans_conn, args=(app_conn, trans_conn)).start()
             while 1:
                 bs = app_conn.recv(1024)
                 if len(bs) == 0:
                     raise Exception("EOF")
                 trans_conn.send(bs)
         except BaseException as e:
-            self.logger.error(
-                "closing accept {}:{} <-> {}:{}, {}".format(app_conn_addr[0], app_conn_addr[1], self.local_host,
-                                                            self.local_port, e))
+            self._logger.error(
+                "closing accept {}:{} <-> {}:{}, {}".format(app_conn_addr[0], app_conn_addr[1], self._local_host,
+                                                            self._local_port, e))
             try:
-                self.logger.error(
-                    "closing relay {}:{} <-> {}:{}, {}".format(trans_conn_addr[0], trans_conn_addr[1], self.server_host,
-                                                               self.server_port, e))
+                self._logger.error(
+                    "closing relay {}:{} <-> {}:{}, {}".format(trans_conn_addr[0], trans_conn_addr[1],
+                                                               self._server_host,
+                                                               self._server_port, e))
             except BaseException as ee:
                 ...
             try:
@@ -113,7 +115,7 @@ class Cli:
             except BaseException as e:
                 ...
 
-    def __handle_trans_conn(self, app_conn: socket.socket, trans_conn: socket.socket):
+    def _handle_trans_conn(self, app_conn: socket.socket, trans_conn: socket.socket):
         try:
             while 1:
                 bs = trans_conn.recv(1024)
@@ -155,7 +157,7 @@ if __name__ == "__main__":
     while 1:
         try:
             cli.start()
-        except KeyboardInterrupt as e:
+        except (SystemExit, KeyboardInterrupt) as e:
             raise e
         except BaseException as e:
             logger.info("cli err: {0}".format(traceback.format_exc()))
--- srv.py
+++ srv.py
@@ -11,81 +11,81 @@ import yaml
 
 class Srv:
     def __init__(self, conf):
-        self.conf = conf
-        self.logger = logging.getLogger()
-        self.remote_conns = []
-        self.trans_conns = []
+        self._conf = conf
+        self._logger = logging.getLogger()
+        self._remote_conns = []
+        self._trans_conns = []
         try:
-            self.server_host = self.conf["server"]["host"]
+            self._server_host = self._conf["server"]["host"]
         except Exception as e:
-            self.logger.info("get server host from config fail: {0}".format(e))
-            raise e
+            raise SystemExit(e)
         try:
-            self.server_port = self.conf["server"]["port"]
+            self._server_port = self._conf["server"]["port"]
         except Exception as e:
-            self.logger.info("get server port from config fail: {0}".format(e))
-            raise e
+            raise SystemExit(e)
         try:
-            self.local_host = self.conf["local"]["host"]
+            self._local_host = self._conf["local"]["host"]
         except Exception as e:
-            self.logger.info("get local host from config fail: {0}".format(e))
-            raise e
+            raise SystemExit(e)
         try:
-            self.local_port = self.conf["local"]["port"]
+            self._local_port = self._conf["local"]["port"]
         except Exception as e:
-            self.logger.info("get local port from config fail: {0}".format(e))
-            raise e
+            raise SystemExit(e)
 
     def __str__(self):
-        return str(self.conf)
+        return str(self._conf)
 
     def start(self):
-        self.logger.info("start server!")
+        self._logger.info("start server")
         listen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        listen.bind((self.local_host, self.local_port))
+        listen.bind((self._local_host, self._local_port))
         listen.listen()
+        self._logger.info(
+            "listen trans conn {}:{}".format(self._local_host, self._local_port))
         while 1:
             try:
                 trans_conn, addr = listen.accept()
-                threading.Thread(target=self.__handle_trans_conn, args=(trans_conn,)).start()
+                threading.Thread(target=self._handle_trans_conn, args=(trans_conn,)).start()
             except BaseException as e:
-                self.logger.error("listen accept trans_conn err: {0}".format(e))
+                self._logger.error(
+                    "closing listen trans conn {}:{}, {}".format(self._local_host, self._local_port, e))
                 try:
                     listen.shutdown(socket.SHUT_RDWR)
                     listen.close()
                 except BaseException as ee:
                     ...
-                self.__when_listen_close()
+                self._when_listen_close()
                 raise e
 
-    def __handle_trans_conn(self, trans_conn: socket.socket):
+    def _handle_trans_conn(self, trans_conn: socket.socket):
         trans_conn_addr = trans_conn.getpeername()
         remote_conn = socket.socket()
         try:
             remote_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            remote_conn.connect((self.server_host, self.server_port))
+            remote_conn.connect((self._server_host, self._server_port))
             remote_conn_addr = remote_conn.getsockname()
-            self.remote_conns.append(remote_conn)
-            self.logger.info("accept {}:{} <-> {}:{}".format(trans_conn_addr[0], trans_conn_addr[1], self.local_host,
-                                                             self.local_port))
-            self.logger.info("relay {}:{} <-> {}:{}".format(remote_conn_addr[0], remote_conn_addr[1], self.server_host,
-                                                            self.server_port))
-            threading.Thread(target=self.__handle_remote_conn, args=(trans_conn, remote_conn)).start()
+            self._remote_conns.append(remote_conn)
+            self._logger.info("accept {}:{} <-> {}:{}".format(trans_conn_addr[0], trans_conn_addr[1], self._local_host,
+                                                              self._local_port))
+            self._logger.info(
+                "relay {}:{} <-> {}:{}".format(remote_conn_addr[0], remote_conn_addr[1], self._server_host,
+                                               self._server_port))
+            threading.Thread(target=self._handle_remote_conn, args=(trans_conn, remote_conn)).start()
             while 1:
                 bs = trans_conn.recv(1024)
                 if len(bs) == 0:
                     raise Exception("EOF")
                 remote_conn.send(bs)
         except BaseException as e:
-            self.logger.error(
-                "closing accept {}:{} <-> {}:{}, {}".format(trans_conn_addr[0], trans_conn_addr[1], self.local_host,
-                                                            self.local_port, e))
+            self._logger.error(
+                "closing accept {}:{} <-> {}:{}, {}".format(trans_conn_addr[0], trans_conn_addr[1], self._local_host,
+                                                            self._local_port, e))
             try:
-                self.logger.error(
+                self._logger.error(
                     "closing relay {}:{} <-> {}:{}, {}".format(remote_conn_addr[0], remote_conn_addr[1],
-                                                               self.server_host,
-                                                               self.server_port, e))
+                                                               self._server_host,
+                                                               self._server_port, e))
             except BaseException as ee:
                 ...
             try:
@@ -99,7 +99,7 @@ class Srv:
             except BaseException as ee:
                 ...
 
-    def __handle_remote_conn(self, trans_conn: socket.socket, remote_conn: socket.socket):
+    def _handle_remote_conn(self, trans_conn: socket.socket, remote_conn: socket.socket):
         try:
             while 1:
                 bs = remote_conn.recv(1024)
@@ -118,21 +118,21 @@ class Srv:
             except BaseException as e:
                 ...
 
-    def __when_listen_close(self):
-        for remote_conn in self.remote_conns:
+    def _when_listen_close(self):
+        for remote_conn in self._remote_conns:
             try:
                 remote_conn.shutdown(socket.SHUT_RDWR)
                 remote_conn.close()
             except BaseException as e:
                 ...
-        self.remote_conns = []
-        for trans_conn in self.trans_conns:
+        self._remote_conns = []
+        for trans_conn in self._trans_conns:
             try:
                 trans_conn.shutdown(socket.SHUT_RDWR)
                 trans_conn.close()
             except BaseException as e:
                 ...
-        self.trans_conns = []
+        self._trans_conns = []
 
 
 if __name__ == "__main__":
@@ -153,13 +153,13 @@ if __name__ == "__main__":
                         format='%(asctime)s %(levelname)s %(pathname)s:%(lineno)d %(thread)s %(message)s')
     logger = logging.getLogger()
     srv = Srv(conf)
-    logger.info("server info: {0}!".format(srv))
+    logger.info("server info: {0}".format(srv))
     while 1:
         try:
             srv.start()
-        except KeyboardInterrupt as e:
+        except (SystemExit, KeyboardInterrupt) as e:
             raise e
         except BaseException as e:
-            logger.info("server err:{0} {1}!".format(e, traceback.format_exc()))
+            logger.info("server err:{0} {1}".format(e, traceback.format_exc()))
             logger.info("server will start in 5s...")
             time.sleep(5)
